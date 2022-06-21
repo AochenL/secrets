dd9e9954393b47933b37cf3268221cf04ab67a1d
Aochen <aochen@pku.edu.cn>
2022-06-21 14:42:45 +0800

    Level 3 Hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+var md5 = require('md5');
 
 const app = express();
 
@@ -12,76 +12,73 @@ mongoose.connect('mongodb://localhost:27017/usersDB');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
     password: {
-        type:String,
-        required:true
+        type: String,
+        required: true
     }
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"] });
-
 const User = mongoose.model('User', userSchema);
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
     res.render("login");
 });
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
     res.render("register");
 });
 
-app.get("/logout",function(req,res){
+app.get("/logout", function(req, res) {
     res.render("home");
 });
 
-app.post("/register",function(req,res){
+app.post("/register", function(req, res) {
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     const newUser = new User({
-          email: email,
-          password: password
+        email: email,
+        password: password
     });
-    newUser.save(function(err){
-        if (err){
+    newUser.save(function(err) {
+        if (err) {
             console.log(err);
-        }else{
+        } else {
             res.render("secrets");
         }
     })
 
 });
 
-app.post("/login",function(req,res){
+app.post("/login", function(req, res) {
     const email = req.body.username;
-    const password = req.body.password;
-    User.findOne({email:email},function(err,foundUser){
-        if (err){
+    const password = md5(req.body.password);
+    User.findOne({
+        email: email
+    }, function(err, foundUser) {
+        if (err) {
             console.log(err);
-        }else{
-            if (foundUser){
-                foundUser.decrypt(function(err){
-                    if (err){
-                        console.log(err);
-                    }else{
-                        if (foundUser.password === password){
-                            // console.log(foundUser.password);
-                            res.render("secrets");
-                        };
-                    };
-                });
+        } else {
+            if (foundUser) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (foundUser.password === password) {
+                        res.render("secrets");
+                    };;
+                };
             }
         }
     });
@@ -91,5 +88,5 @@ app.post("/login",function(req,res){
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
